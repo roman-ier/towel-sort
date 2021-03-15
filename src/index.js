3a38d237dbde0405c9d273645aef8fb8fde2ff95
Raman Karseka <ier@live.ru>
2021-03-16 00:55:05 +0300

    feat: solution for the task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
+function flatten(arr) {
+    const result = []
 
-// You should implement your task here.
+    arr.forEach((i) => {
+        if (Array.isArray(i)) {
+            result.push(...flatten(i))
+        } else {
+            result.push(i)
+        }
+    })
 
-module.exports = function towelSort (matrix) {
-  return [];
+    return result
+}
+
+module.exports = function towelSort(matrix) {
+
+    if (arguments.length > 0 && matrix.length > 0) {
+       let newArr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    const arr = matrix[i];
+    newArr = newArr.concat(i % 2 ? [...arr].reverse() : arr);
+  }
+  return newArr;
+    } else return [];
 }
